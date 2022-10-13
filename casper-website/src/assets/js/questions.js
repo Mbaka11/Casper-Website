dcd0b6ccf310ab8acf4bbc59b3a31cacfe5f121b
Mbaka11 <90229093+Mbaka11@users.noreply.github.com>
2022-10-13 01:34:29 -0400

    Questions generator done

---
+++ casper-website/src/assets/js/questions.js
@@ -0,0 +1,15 @@
+// const questions = [
+//     {
+//       role: "Vous êtes un enseignant",
+//       scenario: "Des enseignants discutent de la fête des mères qui arrive à grand pas. Ils mentionnent qu'il y a des familles qui ne rentrent pas dans le cadre familial normale, c'est-à-dire une famille avec des enfants, un père et une mère. En effet, il faut considérer les familles monoparentales, les familles adoptives, les familles avec deux pères, les familles avec un père veuf, etc. Les enseignants veulent faire quelque chose pour que tout le monde se sent inclus.",
+//       question_1: "Quelle est ta positon par rapport à cet avis ?",
+//       question_2: "Quelles solutions proposerais-tu ?",
+//       question_3: "Tu penses quoi de cela ?",
+//     }
+//   ];
+  
+// var rand = Math.floor(Math.random() * questions.length);
+
+
+
+document.getElementById("role").innerHTML = "caca";
